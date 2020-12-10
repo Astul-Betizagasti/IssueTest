eb9847a49f2e3d1daaae968e1b635d9b25b4c852
Astul Betizagasti <astul.betizagasti@globant.com>
2020-12-10 18:48:40 -0300

    updated workflow

--- .github/LabelsKeywordsConfig.ps1
+++ .github/LabelsKeywordsConfig.ps1
@@ -1,4 +1,39 @@
 @(
-    @{ Label = 'Revit'; Keywords = @('revit') },
-    @{ Label = 'Advance-Steel'; Keywords = @('advance-steel', 'advancesteel', 'advance steel') }
+    @{ Label = 'Wishlist'; Keywords = @(
+        'wish', 
+        'wishlist', 
+        'improvement', 
+        'useful', 
+        'suggestion', 
+        'enhancement', 
+        'feature', 
+        'request') 
+    },
+
+    @{ Label = 'Revit'; Keywords = @(
+        'revit',
+        'element',
+        'ceiling',
+        'floor',
+        'db',
+        'wall',
+        'curtain',
+        'perspective',
+        'view',
+        'callout',
+        'rail',
+        'family',
+        'roof',
+        'structural',
+        'mep',
+        'duct',
+        'casework',
+        'electrical')
+    },
+    
+    @{ Label = 'Advance-Steel'; Keywords = @(
+        'advance-steel', 
+        'advancesteel', 
+        'advance steel') 
+    }
 )
